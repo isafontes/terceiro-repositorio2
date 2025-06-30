8997b2a55dc37699ba1a1c201bd0845818a42d0a
isafontes <145460451+isafontes@users.noreply.github.com>
2025-06-30 08:18:15 -0300

    30/06

--- main.js
+++ main.js
@@ -24,15 +24,35 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinuscula = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].cheked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].cheked){
+        alfabeto = alfabeto + letrasMinuscula;
+    }
+    if (checkbox[2].cheked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].cheked){
+        alfabeto = alfabeto + simbolos;
+    }
+
     let senha = '' ;
     for(let i=0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha; 
 }
